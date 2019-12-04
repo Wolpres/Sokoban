a375bacfb1035d677c5ddd2e34b8de100e36fd85
Josef Dostal <josefdostal@hotmail.com>
2019-12-04 07:57:40 +0100

    minor changes

--- app/src/main/java/com/example/sokoban/GameActivity.java
+++ app/src/main/java/com/example/sokoban/GameActivity.java
@@ -1,18 +1,16 @@
 package com.example.sokoban;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
-    View wonScreen;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-        wonScreen = findViewById(R.id.won_screen);
-    }
+	View wonScreen;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+		wonScreen = findViewById(R.id.won_screen);
+	}
 }
